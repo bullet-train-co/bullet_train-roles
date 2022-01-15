2ec8e369fc0027ccbadda9e0de82b08e2896efe7
Prabin Poudel <probnpoudel@gmail.com>
2022-01-16 01:36:17 +0545

    Features/ Handle default json value for role_ids in mysql
    Tasks
    ---
    
    - Update USAGE file to include new changes
    - Generate migration file only if database is postgresql
    - Add after_initialize callback to top_level_model if database is not postgresql
    - Fix issue of permit line being duplicated due to the addition of 'if user.present?' in previous change

--- lib/generators/bullet_train/roles/install/install_generator.rb
+++ lib/generators/bullet_train/roles/install/install_generator.rb
@@ -8,6 +8,8 @@ module BulletTrain
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("templates", __dir__)
 
+      desc "Create config/models/roles.yml with default roles"
+
       def add_roles_config_file
         puts("Creating role.yml inside config/models with default roles\n")
 
@@ -16,22 +18,38 @@ module BulletTrain
         puts("Success 🎉🎉\n\n")
       end
 
-      def configure_models
-        json_data_type_identifier = find_json_data_type_identifier
+      desc "Configure models to support roles"
 
+      def configure_models
         top_level_model = ask("Which model do you want to use as a top-level model that represents Membership? (Defaults to Membership)") || "Membership"
 
-        generate_migration_to_add_role_ids(top_level_model, json_data_type_identifier)
+        add_role_ids_to_top_level_model(top_level_model)
 
         include_role_support_in_top_level_model(top_level_model)
 
         associated_model = ask("Which model/association of #{top_level_model} do you consider to be the Team? (Default to Team)") || "Team"
 
-        # Asks for a model we can setup some default permissions for.
-        # TODO: follow back Andrew on question about this in Slack
-
+        include_roles_permit_in_ability_model
         add_permit_to_ability_model(top_level_model, associated_model)
-        add_concern_to_user
+      end
+
+      desc "Add 'include Roles::User' to User"
+
+      def add_roles_user_concern_to_user
+        file_location = "app/models/user.rb"
+        line_to_match = "class User < ApplicationRecord"
+        content_to_add = "\n  include Roles::User\n"
+
+        puts("\nAdding 'include Roles::User' to User\n\n")
+
+        if line_exists_in_file?(file_location, content_to_add)
+          message = "#{remove_new_lines_and_spaces(content_to_add)} already exists in User!!\n\n"
+          line_already_exists(message)
+        else
+          add_in_file(file_location, line_to_match, content_to_add)
+        end
+
+        puts("\nSuccess 🎉🎉\n\n")
       end
 
       private
@@ -58,24 +76,17 @@ module BulletTrain
         adapter_name
       end
 
-      def find_json_data_type_identifier
-        adapter_name = db_adapter
-
-        case adapter_name
-        when "postgresql"
-          "jsonb"
-        else
-          "json"
-        end
+      def postgresql_database?
+        db_adapter == 'postgresql'
       end
 
-      def add_in_file(file_location, line_to_match, content_to_add)
+      def add_in_file(file_location, add_content_after, content_to_add)
         update_file_content = []
 
         file_lines = File.readlines(file_location)
 
         file_lines.each do |line|
-          line_pattern = line_to_match
+          line_pattern = add_content_after
           updated_line = line
 
           if line.include?(line_pattern)
@@ -108,7 +119,7 @@ module BulletTrain
         puts(message)
       end
 
-      def generate_migration_to_add_role_ids(top_level_model, json_data_type_identifier)
+      def generate_migration_to_add_role_ids(top_level_model)
         top_level_model_table_name = top_level_model.underscore.pluralize
 
         migration_file_name = "add_role_ids_to_#{top_level_model_table_name}"
@@ -117,9 +128,28 @@ module BulletTrain
 
         puts("Generating migration to add role_ids to #{top_level_model}")
 
-        generate "migration", "#{migration_file_name} role_ids:#{json_data_type_identifier}"
+        generate "migration", "#{migration_file_name} role_ids:jsonb"
 
         add_default_value_to_migration(migration_file_name, top_level_model_table_name)
+
+        puts("Success 🎉🎉\n\n")
+      end
+
+      def add_set_default_role_ids_to_model(model_name)
+        file_location = "app/models/#{model_name.underscore}.rb"
+
+        line_to_match = "class #{model_name.classify}"
+        content_to_add = "\nafter_initialize :set_default_role_ids\n\ndef set_default_role_ids\n  self.role_ids ||= []\nend\n\n"
+
+        puts("Adding 'set_default_role_ids' callback to #{model_name}\n\n")
+
+        if line_exists_in_file?(file_location, content_to_add)
+          message = "'#{remove_new_lines_and_spaces(content_to_add)}' already exists in #{model_name}!!\n\n"
+
+          return line_already_exists(message)
+        end
+
+        add_in_file(file_location, line_to_match, content_to_add)
 
         puts("Success 🎉🎉\n\n")
       end
@@ -136,6 +166,14 @@ module BulletTrain
 
       def remove_new_lines_and_spaces(line)
         line.tr("\n", "").strip
+      end
+
+      def add_role_ids_to_top_level_model(top_level_model)
+        if postgresql_database?
+          generate_migration_to_add_role_ids(top_level_model)
+        else
+          add_set_default_role_ids_to_model(top_level_model)
+        end
       end
 
       def include_role_support_in_top_level_model(model_name)
@@ -160,26 +198,17 @@ module BulletTrain
         puts("Success 🎉🎉\n\n")
       end
 
-      def add_permit_to_ability_model(top_level_model, associated_model)
+      def include_roles_permit_in_ability_model
         file_location = "app/models/ability.rb"
         line_to_match = "include CanCan::Ability"
         content_to_add = "\n  include Roles::Permit\n"
-        puts("Adding 'include Roles::Permit' to #{associated_model}\n\n")
+
+        puts("Adding 'include Roles::Permit' to 'app/models/ability.rb'\n\n")
 
         if line_exists_in_file?(file_location, content_to_add)
-          message = "#{remove_new_lines_and_spaces(content_to_add)} already exists in #{associated_model}!!\n\n"
-          line_already_exists(message)
-        else
-          add_in_file(file_location, line_to_match, content_to_add)
-        end
+          message = "#{remove_new_lines_and_spaces(content_to_add)} already exists in 'app/models/ability.rb'!!\n\n"
 
-        line_to_match = "def initialize(user)"
-        content_to_add = "\n    permit user, through: :#{top_level_model.downcase.pluralize}, parent: :#{associated_model.downcase} if user.present?\n"
-        puts("Adding 'permit' to #{associated_model}\n\n")
-
-        if line_exists_in_file?(file_location, content_to_add)
-          message = "#{remove_new_lines_and_spaces(content_to_add)} already exists in #{associated_model}!!\n\n"
-          line_already_exists(message)
+          return line_already_exists(message)
         else
           add_in_file(file_location, line_to_match, content_to_add)
         end
@@ -187,17 +216,20 @@ module BulletTrain
         puts("Success 🎉🎉\n\n")
       end
 
-      def add_concern_to_user
-        file_location = "app/models/user.rb"
-        line_to_match = "class User < ApplicationRecord"
-        content_to_add = "\n  include Roles::User\n"
-        puts("Adding 'include Roles::User' to User\n\n")
+      def add_permit_to_ability_model(top_level_model, associated_model)
+        file_location = "app/models/ability.rb"
+        add_content_after = "def initialize(user)"
+        line_to_compare = "permit user, through: :#{top_level_model.downcase.pluralize}, parent: :#{associated_model.downcase}"
+        content_to_add = "\n    #{line_to_compare} if user.present?\n"
 
-        if line_exists_in_file?(file_location, content_to_add)
-          message = "#{remove_new_lines_and_spaces(content_to_add)} already exists in User!!\n\n"
-          line_already_exists(message)
+        puts("Adding '#{remove_new_lines_and_spaces(content_to_add)}' to 'app/models/ability.rb'\n\n")
+
+        if line_exists_in_file?(file_location, line_to_compare)
+          message = "'#{remove_new_lines_and_spaces(content_to_add)}' already exists in 'app/models/ability.rb'!!\n\n"
+
+          return line_already_exists(message)
         else
-          add_in_file(file_location, line_to_match, content_to_add)
+          add_in_file(file_location, add_content_after, content_to_add)
         end
 
         puts("Success 🎉🎉\n\n")
