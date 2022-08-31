2248fc16b237547a508658e08322ca66ac57c165
Dan Singerman <dan@reasonfactory.com>
2022-08-31 15:52:27 +0100

    save permissions structure to Rails.cache if a cache key is passed

--- lib/roles/permit.rb
+++ lib/roles/permit.rb
@@ -2,45 +2,67 @@
 
 module Roles
   module Permit
-    def permit(user, through:, parent:, debug: false, intermediary: nil)
+    def permit(user, through:, parent:, debug: false, intermediary: nil, cache_key: nil)
       # When changing permissions during development, you may also want to do this on each request:
       # User.update_all ability_cache: nil if Rails.env.development?
-      output = []
-      added_roles = Set.new
-      user.send(through).map(&:roles).flatten.uniq.each do |role|
-        unless added_roles.include?(role)
-          output << "########### ROLE: #{role.key}"
-          output += add_abilities_for(role, user, through, parent, intermediary)
-          added_roles << role
+      permissions = if cache_key
+        Rails.cache.fetch(cache_key) do
+          build_permissions(user, through, parent, intermediary)
         end
+      else
+        build_permissions(user, through, parent, intermediary)
+      end
 
-        role.included_roles.each do |included_role|
-          unless added_roles.include?(included_role)
-            output << "############# INCLUDED ROLE: #{included_role.key}"
-            output += add_abilities_for(included_role, user, through, parent, intermediary)
-          end
-        end
+      permissions.each do |permission|
+        can(permission[1], permission[2].constantize, permission[3]) if permission[0]
       end
 
       if debug
         puts "###########################"
         puts "Auto generated `ability.rb` content:"
-        puts output
+        permissions.map do |permission|
+          if permission[0]
+            puts  "can #{permission[1]}, #{permission[2]}, #{permission[3]}"
+          else
+            puts permission[1]
+          end
+        end
         puts "############################"
       end
     end
 
-    def add_abilities_for(role, user, through, parent, intermediary)
-      output = []
-      role.ability_generator(user, through, parent, intermediary) do |ag|
-        if ag.valid?
-          output << "can #{ag.actions}, #{ag.model}, #{ag.condition}"
-          can(ag.actions, ag.model, ag.condition)
-        else
-          output << "# #{ag.model} does not respond to #{parent} so we're not going to add an ability for the #{through} context"
+    def build_permissions(user, through, parent, intermediary)
+      added_roles = Set.new
+      permissions = []
+      user.send(through).map(&:roles).flatten.uniq.each do |role|
+        unless added_roles.include?(role)
+          permissions << [false, "########### ROLE: #{role.key}"]
+          permissions += add_abilities_for(role, user, through, parent, intermediary)
+          added_roles << role
+        end
+
+        role.included_roles.each do |included_role|
+          unless added_roles.include?(included_role)
+            permissions << [false, "############# INCLUDED ROLE: #{included_role.key}"]
+            permissions += add_abilities_for(included_role, user, through, parent, intermediary)
+          end
         end
       end
-      output
+
+      permissions
+    end
+
+    def add_abilities_for(role, user, through, parent, intermediary)
+      output = []
+      permissions = []
+      role.ability_generator(user, through, parent, intermediary) do |ag|
+        if ag.valid?
+          permissions << [true, ag.actions, ag.model.to_s, ag.condition]
+        else
+          permissions << [false, "# #{ag.model} does not respond to #{parent} so we're not going to add an ability for the #{through} context"]
+        end
+      end
+      permissions
     end
   end
 end
